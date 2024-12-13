eb0befebed6f426c2e0d0a8781ca5626c5b61a54
Ashutosh Kumar Singh <ashuhar9450@gmail.com>
2024-12-13 12:52:05 +0530

    Added calendar,contact page
    -Add contact.html
    -Add Book.html
    -Add calendar.css
    -Add calendar.js
    
    Signed-off-by: Ashutosh Kumar Singh <ashuhar9450@gmail.com>

---
+++ Script.js
@@ -0,0 +1,9 @@
+let btn=document.getElementById("btn");
+let contact=document.getElementById("contact");
+
+btn.addEventListener('click',(e)=>{
+    window.location.href = "Book.html"
+})
+contact.addEventListener("click",(e)=>{
+    window.location.href="Contact.html"
+})
