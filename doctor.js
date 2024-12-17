ea6f13e586738d3aa67bdc8b97e21414a06deb58
Akshatb2006 <kysuakshat23@gmail.com>
2024-12-17 16:32:37 +0530

    Add files via upload
    image of the back button
    doctor page showing the list of doctors
    its css and its js

---
+++ doctor.js
@@ -0,0 +1,6 @@
+let btns = document.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+     btns[i].addEventListener('click', (e) => {
+         window.location.href = "Book.html"; 
+        }); 
+    }
