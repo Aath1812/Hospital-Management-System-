64c8b8ff1015bb5a16ff86ff6c4aeb2b28d90ed9
Aath1812 <atharvababhulgaonkar334@gmail.com>
2024-12-17 16:05:36 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-let btn=document.getElementById("btn");
-let contact=document.getElementById("contact");
-
-btn.addEventListener('click',(e)=>{
-    window.location.href = "Book.html"
-})
-contact.addEventListener("click",(e)=>{
-    window.location.href="Contact.html"
-})
